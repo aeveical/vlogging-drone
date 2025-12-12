602cd1f6826d0dec62aeba24ca41af5719a85568
Liam Fries <lfries@olin.edu>
2025-12-12 11:44:18 -0500

    kinda implemented mavlink in main but not rlly tho

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 from drone_directions import directions
 from hover_test import Hover
+from mavlink_test import Mavlink
 ## MAIN
 
 TARGET_ALT = 1  # meters
 SERIAL_CONNECTION = 123
 autonomous = False
-
-
-def start():
-    # import hover code class
-    
-    drone = Hover("udp:127.0.0.1:14550")
-    drone.wait_heartbeat()
-    drone.set_mode("GUIDED")
-    drone.arm()
-    drone.takeoff(5)
-    drone.hover(10)
-#    drone.land()
-
+DRONE_PATH = ''
 # ---- MAIN ---- #
 while autonomous == True:
-    start()
+    drone_hover = Hover(DRONE_PATH)
+    drone_hover.start()
     main_directions = directions(0, 0, 0, 0, 0, 0, 0, 0) #imports all the stats starting at 0
     main_directions.start_cam()
     main_directions.get_directions()
+    drone_autonomous = Mavlink(DRONE_PATH, 0, 0, 0)
+
 
 #    drone.land()
--- mavlink_test.py
+++ mavlink_test.py
@@ -1,27 +1,19 @@
 import serial
 from pymavlink import mavutil
 
-vel = 1
-accel = 0.5
-turn_rate = 10
-connection = mavutil.mavlink_connection('/dev/ttyTHS1', baud=57600)
+class Mavlink:
 
-def approach_target_rc_override(roll, pitch, throttle, yaw):
-    connection.mav.rc_channels_override_send(
-    connection.target_system,
-    connection.target_component,
-    roll, pitch, throttle, yaw, 
-    0, 0, 0, 0         
-    )
+    def __init__(self, serial, yaw_angle, pitch, throttle):
+        self.yaw_angle = yaw_angle
+        self.pitch = pitch
+        self.throttle = throttle
+        self.master = mavutil.mavlink_connection(serial) #baud rate??
 
-def approach_target_set_point(x, y, z, yaw):
-    connection.mav.set_position_target_local_ned_send(
-    0, connection.target_system, connection.target_component,
-    mavutil.mavlink.MAV_FRAME_LOCAL_NED,
-    x, y, z,             # x, y, z positions
-    vel, vel, vel,             # vx, vy, vz
-    accel, accel, accel,             # ax, ay, az
-    yaw,                # yaw (radians)
-    turn_rate                    # yaw rate
-)
-    
+    def approach_target_rc_override(self):
+        self.master.mav.rc_channels_override_send(
+        self.master.target_system,
+        self.master.target_component,
+        0, self.pitch, self.throttle, self.yaw_angle, 
+        0, 0, 0, 0         
+        )
+
